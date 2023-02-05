b331f18e1318b469719b5b24a33f61af2d2a5003
Tolkinxon <krosstony639@gmail.com>
2023-02-05 17:14:04 +0500

    project is working

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import 'tailwindcss/tailwind.css'
 import '@/styles/globals.scss'
+import { Layout } from '@/components'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
