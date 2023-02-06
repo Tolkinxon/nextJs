15fea26953219773f498793079c0d7db198ac7e4
Tolkinxon <krosstony639@gmail.com>
2023-02-06 21:32:24 +0500

    added api graphcms

--- services/index.js
+++ services/index.js
@@ -1,37 +1,37 @@
 import { request, gql } from 'graphql-request'
 
 export const getPost = async () => {
-    
+  
+  const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 
   const query = gql`
- query MyQuery {
-  postsConnection {
-    edges {
-      node {
-        createdAt
-        author {
-          bio
-          name
-          id
-          photo {
-            url
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            createdAt
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
           }
-        }
-        slug
-        title
-        excerpt
-        featuredImage {
-          url
-        }
-        categories {
-          name
-          slug
         }
       }
     }
-  }
-}
-
   `
 
   const result = await request(graphqlAPI, query)
