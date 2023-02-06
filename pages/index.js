58e75434d9359dc03b89e7b57e230d534550a4d4
Tolkinxon <krosstony639@gmail.com>
2023-02-06 23:40:27 +0500

    post widger changed to right

--- pages/index.js
+++ pages/index.js
@@ -14,16 +14,18 @@ export default function Home({ posts }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky realtive top-8  ">
+            <div className="lg:sticky relative top-8">
+              <Categories />
+              <PostWidget />
+            </div>
+          </div>
+        </div>
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
             <PostCard post={post.node} key={post.title} />
           ))}
-        </div>
-      </div>
-      <div className="lg:col-span-4 col-span-1">
-        <div className="lg:sticky relative top-8">
-          <Categories />
-          <PostWidget />
         </div>
       </div>
     </div>
