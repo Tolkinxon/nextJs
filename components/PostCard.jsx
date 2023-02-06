fe4b0b3e5b838162ec54f2a2e21950a546ae471e
Tolkinxon <krosstony639@gmail.com>
2023-02-06 22:49:06 +0500

    added new button

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -17,7 +17,27 @@ const PostCard = ({ post }) => {
             height="30px"
             className="align-middle rounded-full"
           />
-          <p>{post.author.name}</p>
+          <p className="inline align-middle text-gray-700 ml-2 text-lg ">
+            {post.author.name}
+          </p>
+          <span>&nbsp; &nbsp;</span>
+          <div className="font-medium text-gray-700">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 inline mr-2 text-pink-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+              />
+            </svg>
+            <span>{moment(post.createdAt).format('MMM DD, YYYY')}</span>
+          </div>
         </div>
       </div>
       <div className="relative overflow-hidden shadow-md pb-80 mb-6">
@@ -26,6 +46,16 @@ const PostCard = ({ post }) => {
           alt={post.title}
           className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rouded-lg"
         />
+      </div>
+      <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
+        {post.excerpt}
+      </p>
+      <div className="text-center">
+        <Link href={`/post/${post.slug}`}>
+          <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-indigo-800 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
+            Cotinue Reading
+          </span>
+        </Link>
       </div>
     </div>
   )
