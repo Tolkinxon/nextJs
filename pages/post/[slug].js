3539b8930463714c6a67c7092a74d56ad4c4b1e5
Tolkinxon <krosstony639@gmail.com>
2023-02-08 07:50:46 +0500

    added /post/slugs

---
+++ pages/post/[slug].js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const postDetails = () => {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="col-span-1 lg:col-span-4"></div>
+        <div className="col-span-1 lg:col-span-8"></div>
+      </div>
+    </div>
+  )
+}
+
+export default postDetails
